0b9c05dead057719f9180082c089ad77a909c3bc
gizmore <gizmore@wechall.net>
2019-06-30 16:08:53 +0200

    6.09-r890_init

---
+++ Module_Bootstrap3.php
@@ -0,0 +1,19 @@
+<?php
+namespace GDO\Bootstrap;
+
+use GDO\Core\GDO_Module;
+use GDO\Core\Module_Core;
+
+final class Module_Bootstrap3 extends GDO_Module
+{
+	public $module_priority = 10;
+	
+	public function onIncludeScripts()
+	{
+		$min = Module_Core::instance()->cfgMinifyJS() === 'no' ? '' : '.min';
+
+		$this->addBowerJavascript("bootstrap/dist/js/bootstrap$min.js");
+		$this->addBowerCSS("bootstrap/dist/css/bootstrap$min.css");
+	}
+	
+}
