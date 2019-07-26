c120e34c45a42a269c3b098838f83b158dc7ab84
gizmore <gizmore@wechall.net>
2019-07-27 01:01:34 +0200

    6.09-r912_dependencies

--- Module_Bootstrap3.php
+++ Module_Bootstrap3.php
@@ -8,6 +8,8 @@ final class Module_Bootstrap3 extends GDO_Module
 {
 	public $module_priority = 10;
 	
+	public function getDependencies() { return ['JQuery']; }
+	
 	public function onIncludeScripts()
 	{
 		$min = Module_Core::instance()->cfgMinifyJS() === 'no' ? '' : '.min';
