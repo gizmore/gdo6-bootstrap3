f789273822a23e1524a66b47aaf416c0561986bd
gizmore <gizmore@wechall.net>
2021-03-07 03:28:07 +0100

    6.10.1-r1011_themeswitching

--- Module_Bootstrap3.php
+++ Module_Bootstrap3.php
@@ -3,19 +3,32 @@ namespace GDO\Bootstrap3;
 
 use GDO\Core\GDO_Module;
 use GDO\Core\Module_Core;
+use GDO\Core\Application;
 
+/**
+ * Bootstrap3 asset files.
+ * Make sure your theme chain includes bs3 to get them loaded.
+ * @author gizmore
+ * @version 6.10.1
+ * @since 4.0.2
+ */
 final class Module_Bootstrap3 extends GDO_Module
 {
 	public $module_priority = 10;
 	
 	public function getDependencies() { return ['JQuery']; }
 	
+	public function getTheme() { return 'bs3'; }
+	
 	public function onIncludeScripts()
 	{
-		$min = Module_Core::instance()->cfgMinifyJS() === 'no' ? '' : '.min';
-
-		$this->addBowerJavascript("bootstrap/dist/js/bootstrap$min.js");
-		$this->addBowerCSS("bootstrap/dist/css/bootstrap$min.css");
+	    if (Application::instance()->hasTheme('bs3'))
+	    {
+    		$min = Module_Core::instance()->jsMinAppend();
+    		
+    		$this->addBowerJavascript("bootstrap/dist/js/bootstrap$min.js");
+    		$this->addBowerCSS("bootstrap/dist/css/bootstrap$min.css");
+	    }
 	}
 	
 }
